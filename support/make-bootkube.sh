dcc3be53a6c477f538a00b4568420e5fab71dd53
Salman Malik <smalik@pivotal.io>
2018-09-11 16:32:13 -0400

    Update user-data to allow rook.io to work

--- support/make-bootkube.sh
+++ support/make-bootkube.sh
@@ -1,11 +1,21 @@
 #!/bin/bash -x
 
+# compile bootkube binaries
 go get -u github.com/kubernetes-incubator/bootkube
 cd $(go env GOPATH | cut -d: -f1)/src/github.com/kubernetes-incubator/bootkube
 make cross
 
+# copy over the bootkube artifacts
 cd ..
-mv bootkube /tmp/external/kubernetes-incubator-bootkube
-cp -r /tmp/external/support/rook.io /tmp/external/kubernetes-incubator-bootkube/hack/
-cp /tmp/external/support/Vagrantfile /tmp/external/kubernetes-incubator-bootkube/hack/multi-node/
-cp /tmp/external/support/bootkube-up /tmp/external/kubernetes-incubator-bootkube/hack/multi-node/
+BOOTKUBE_DIR='/tmp/external/kubernetes-incubator-bootkube'
+mv bootkube ${BOOTKUBE_DIR}
+
+# override some bootkube files
+cp /tmp/external/support/Vagrantfile ${BOOTKUBE_DIR}/hack/multi-node/
+cp /tmp/external/support/bootkube-up ${BOOTKUBE_DIR}/hack/multi-node/
+
+# copy over rook.io resources for persistent storage
+cp -r /tmp/external/support/rook.io ${BOOTKUBE_DIR}/hack/
+
+# enable the flex-volume plugin needed by rook.io
+sed -i 's|--rotate-certificates|--rotate-certificates --volume-plugin-dir=/var/lib/kubelet/volumeplugins|' ${BOOTKUBE_DIR}/hack/multi-node/user-data.sample
