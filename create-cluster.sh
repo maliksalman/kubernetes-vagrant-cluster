c70cd277d66f3c8a9ce648b0c74655f4ed070c6d
Salman Malik <smalik@pivotal.io>
2019-01-12 00:08:21 -0500

    Updating to latest dashboard. Run docker as non-root to make bootkube

--- create-cluster.sh
+++ create-cluster.sh
@@ -5,7 +5,7 @@ rm -rf kubernetes-incubator-bootkube
 rm -f cluster
 
 # create docker image that will ready bootkube
-docker run -i -t -v $PWD:/tmp/external golang /tmp/external/support/make-bootkube.sh
+docker run --user $(id -u):$(id -g) -e 'XDG_CACHE_HOME=/tmp/.cache' -i -t -v $PWD:/tmp/external golang /tmp/external/support/make-bootkube.sh
 
 # run bootkube
 (cd kubernetes-incubator-bootkube/hack/multi-node; ./bootkube-up)
